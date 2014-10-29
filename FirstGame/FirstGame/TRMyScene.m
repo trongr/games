690f03319a3c9092574a385d699708d9d5668b4a
Trong Truong <nahn.truong@gmail.com>
2014-10-29 19:38:22 -0400

    character physics and collision

--- FirstGame/FirstGame/TRMyScene.m
+++ FirstGame/FirstGame/TRMyScene.m
@@ -33,8 +33,14 @@ const int NUMGROUND = 2; // more if the screen is much larger than the backgroun
 const float GROUNDSCALE = 0.35;
 const int MOVEGROUNDDURATION = 5;
 
+NSString* const PLAYER = @"player";
+const int PLAYER_WIDTH = 50;
+
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
+        self.physicsWorld.gravity = CGVectorMake(0.0, -5.0);
+        self.physicsWorld.contactDelegate = self;
+        
         _bglayer = [SKNode node];
         [self addChild:_bglayer];
         _gamelayer = [SKNode node];
@@ -62,7 +68,7 @@ const int MOVEGROUNDDURATION = 5;
         for (int i=0; i < NUMGROUND; i++) {
             SKSpriteNode* sprite = [SKSpriteNode spriteNodeWithTexture:ground];
             [sprite setScale:GROUNDSCALE];
-            sprite.zPosition = -1;
+            sprite.zPosition = 0;
             sprite.anchorPoint = CGPointZero;
             sprite.position = CGPointMake(i * sprite.size.width, 0);
             
@@ -74,26 +80,38 @@ const int MOVEGROUNDDURATION = 5;
             [_gamelayer addChild:sprite];
         }
 
+        _player = [SKSpriteNode spriteNodeWithImageNamed:PLAYER];
+        _player.position = CGPointMake(60, 400);
+        _player.zPosition = 0;
+        _player.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(PLAYER_WIDTH, PLAYER_WIDTH)];
+        _player.physicsBody.dynamic = YES;
+        _player.physicsBody.allowsRotation = NO;
+        _player.physicsBody.categoryBitMask = planecategory;
+        _player.physicsBody.collisionBitMask = worldcategory | pipecategory | skycategory;
+        _player.physicsBody.contactTestBitMask = worldcategory | pipecategory | skycategory;
+        [_gamelayer addChild:_player];
+        
+        SKNode* dummyground = [SKNode node];
+        dummyground.position = CGPointMake(0, ground.size.height * GROUNDSCALE);
+        dummyground.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.frame.size.width, 1)];
+        dummyground.physicsBody.dynamic = NO;
+        dummyground.physicsBody.categoryBitMask = worldcategory;
+        [_gamelayer addChild:dummyground];
+ 
+        SKNode* dummyceiling = [SKNode node];
+        dummyceiling.position = CGPointMake(0, self.size.height);
+        dummyceiling.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.size.width, 1)];
+        dummyceiling.physicsBody.dynamic = NO;
+        dummyceiling.physicsBody.categoryBitMask = skycategory;
+        [_gamelayer addChild:dummyceiling];
+        
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    /* Called when a touch begins */
-//    
-//    for (UITouch *touch in touches) {
-//        CGPoint location = [touch locationInNode:self];
-//        
-//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-//        
-//        sprite.position = location;
-//        
-//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-//        
-//        [sprite runAction:[SKAction repeatActionForever:action]];
-//        
-//        [self addChild:sprite];
-//    }
+    _player.physicsBody.velocity = CGVectorMake(0, 0);
+    [_player.physicsBody applyImpulse:CGVectorMake(0, 30)];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
