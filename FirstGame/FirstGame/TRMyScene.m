de5c77c3a99d3aaf176593197d120b4cb3134994
Trong Truong <nahn.truong@gmail.com>
2014-10-29 18:47:36 -0400

    parallax side scrolling

--- FirstGame/FirstGame/TRMyScene.m
+++ FirstGame/FirstGame/TRMyScene.m
@@ -14,12 +14,24 @@
     SKNode* _hudlayer;
     NSTimeInterval* _dt;
     NSTimeInterval* _lastupdatetime;
+    SKSpriteNode* _player;
 }
+
+static const uint32_t planecategory = 1 << 0;
+static const uint32_t worldcategory = 1 << 1;
+static const uint32_t pipecategory = 1 << 2;
+static const uint32_t scorecategory = 1 << 3;
+static const uint32_t skycategory = 1 << 4;
 
 NSString* const BG = @"bg";
 const int NUMBG = 2; // more if the screen is much larger than the background width
 const float BGSCALE = 0.65;
-const int MOVEBGDURATION = 5;
+const int MOVEBGDURATION = 50;
+
+NSString* const GROUND = @"ground";
+const int NUMGROUND = 2; // more if the screen is much larger than the background width
+const float GROUNDSCALE = 0.35;
+const int MOVEGROUNDDURATION = 5;
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
@@ -46,6 +58,22 @@ const int MOVEBGDURATION = 5;
             [_bglayer addChild:sprite];
         }
         
+        SKTexture* ground = [SKTexture textureWithImageNamed:GROUND];
+        for (int i=0; i < NUMGROUND; i++) {
+            SKSpriteNode* sprite = [SKSpriteNode spriteNodeWithTexture:ground];
+            [sprite setScale:GROUNDSCALE];
+            sprite.zPosition = -1;
+            sprite.anchorPoint = CGPointZero;
+            sprite.position = CGPointMake(i * sprite.size.width, 0);
+            
+            SKAction* moveground = [SKAction moveByX:-sprite.size.width y:0 duration:MOVEGROUNDDURATION];
+            SKAction* resetground = [SKAction moveByX:sprite.size.width y:0 duration:0];
+            SKAction* movegroundforever = [SKAction repeatActionForever:[SKAction sequence:@[moveground, resetground]]];
+            
+            [sprite runAction:movegroundforever];
+            [_gamelayer addChild:sprite];
+        }
+
     }
     return self;
 }
